30ad7546376dd0b8479ed08b287b1f2608caf9d0
Omayma AKRACH <107046252+OmaymaAKRACH@users.noreply.github.com>
2022-08-15 17:32:36 +0100

    Update script.js
    first commit

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -6,16 +6,3 @@ function toggleButton(){
 }
  hamburgerButton.addEventListener('click',toggleButton);
 
-function sendEmail(){
-  Email.send({
-    Host : "smtp.gmail.com",
-    Username : "omayma.akrach@edu.uiz.ac.ma",
-    Password : "Omayma@123",
-    To : 'omaymaakrach1@gmail.com',
-    From : document.getElementById("email").value,
-    Subject : "new Contact form enquiry",
-    Body : "And this is the body"
-}).then(
-  message => alert(message)
-);
-}
